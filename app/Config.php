a36329ae82eb811a05c2640409b770011b082c9e
mohammed <msb.duck@gmail.com>
2021-11-29 17:42:06 +0100

    hemodificado el config  alas 1742

--- app/Config.php
+++ app/Config.php
@@ -3,7 +3,7 @@
  class Config
  {
      static public $mvc_bd_hostname = "localhost";
-     static public $mvc_bd_nombre   = "mysimonparbasedatos";
+     static public $mvc_bd_nombre   = "alimentosbasedatos";
      static public $mvc_bd_usuario  = "simonsql";
      static public $mvc_bd_clave    = "Msb.93345900";
      static public $mvc_vis_css     = "estilo.css";
